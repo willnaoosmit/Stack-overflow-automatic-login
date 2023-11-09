e3725b5d819532e21313fd2dc0c8543d8e1b7dde
William Brochensque Júnior <55159911+willnaoosmith@users.noreply.github.com>
2023-11-09 08:21:03 -0300

    Made it work with newer selenium versions

--- StackOverflowAutomaticLogin.py
+++ StackOverflowAutomaticLogin.py
@@ -1,29 +1,36 @@
-from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from time import sleep
+import os
 
 user = "YourUserHere"
 password = "YourPasswordHere"
 
 try:
-
 	options = Options()
 	options.add_argument("--headless")
-	browser = webdriver.Firefox(options=options)
+	service = Service(log_path=os.devnull)
+	browser = webdriver.Firefox(options=options, service=service)
 
 	browser.get(r'https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
-	time.sleep(2)
+	sleep(2)
 
-	browser.find_element_by_id('email').send_keys(user)
-	browser.find_element_by_name('password').send_keys(password)
-	time.sleep(1)
+	browser.find_element(By.ID, 'email').send_keys(user)
+	browser.find_element(By.NAME, 'password').send_keys(password)
+	sleep(1)
 
-	browser.find_element_by_name('submit-button').click()
+	browser.find_element(By.NAME, 'submit-button').click()
 	print('logged in')
-	time.sleep(60)
+	sleep(60)
 
 except Exception as error:
+	print(error)
 	browser.close()
 
 finally:
-	browser.close()
+	try:
+		browser.close()
+	except:
+		pass
